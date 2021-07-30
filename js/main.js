c9b6c795f54b7e9045d186a4e753463a8c8ed666
altkov <you@example.com>
2021-07-30 08:39:17 +0300

    0.1.1 index ready

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 jQuery(function($) {
 
+    /* Mobile menu */
+
     let mobileMenu = {
+        menu: '.mobile-menu',
+        opened: '.header .menu-opened',
+        closed: '.header .menu-closed',
+        container: '.header-mobile',
+
         open: function(speed) {
-            this.changeState($('.header .menu-closed'), $('.header .menu-opened'), $('.mobile-menu'), speed);
+            $(this.container).addClass('menu-active');
+            this.changeState($(this.closed), $(this.opened), $(this.menu), speed);
         },
 
         close: function(speed) {
-            this.changeState($('.header .menu-opened'), $('.header .menu-closed'), $('.mobile-menu'), speed);
+            $(this.container).removeClass('menu-active');
+            this.changeState($(this.opened), $(this.closed), $(this.menu), speed);
         },
 
         changeState: function($hide, $show, $menu, speed) {
@@ -24,5 +33,20 @@ jQuery(function($) {
     $('.header .menu-close').on('click', function() {
         mobileMenu.close(400);
     });
+
+
+
+
+    $('.content-tabs .content-tab').on('click', function() {
+        let $tabs = $(this).closest('.content-tabs');
+        $tabs.find('.content-tab').removeClass('active');
+        $($tabs.data('target')).find('.js-tab-content').slideUp(400);
+        let target = $(this).attr('href');
+        $(target).slideDown(400);
+        $(this).addClass('active');
+    });
+
+
+
 
 });
